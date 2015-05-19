b2bea99b6f9d61a39adc06cc3085b4967fb210df
susverwimp <sus.verwimp@hotmail.com>
2015-05-19 15:16:05 +0200

    libs van lwjgl in git

--- lwjgl/src/lwjgl/Main.java
+++ lwjgl/src/lwjgl/Main.java
@@ -6,12 +6,10 @@ import java.util.Random;
 
 import math.Vector3f;
 import models.ModelData;
-import models.RawModel;
 import models.TexturedModel;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
-import shaders.StaticShader;
 import terrains.Terrain;
 import textures.ModelTexture;
 import textures.TerrainTexture;
@@ -19,7 +17,6 @@ import textures.TerrainTexturePack;
 import graphics.Loader;
 import graphics.MasterRenderer;
 import graphics.OBJFileLoader;
-import graphics.EntityRenderer;
 import graphics.Window;
 
 public class Main implements Runnable {
@@ -35,40 +32,79 @@ public class Main implements Runnable {
 
 	@Override
 	public void run() {
+		// initialize glfw window
+
 		Window window = new Window(1280, 720, "Open World");
 		window.init();
 
 		Loader loader = new Loader();
 
-		TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("res/grass-terrain-texture.png"));
-		TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("res/dirt-terrain-texture.png"));
-		TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("res/flowergrass-terrain-texture.png"));
-		TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("res/rock-terrain-texture.png"));
-		
-		TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, bTexture);
-		
-		TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("res/blendmap.png"));
-		
+		// create terrain textures and blendmap, used for multitexturing
+		// terrains
+
+		TerrainTexture backgroundTexture = new TerrainTexture(
+				loader.loadTexture("res/grass-terrain-texture.png"));
+		TerrainTexture rTexture = new TerrainTexture(
+				loader.loadTexture("res/dirt-terrain-texture.png"));
+		TerrainTexture gTexture = new TerrainTexture(
+				loader.loadTexture("res/flowergrass-terrain-texture.png"));
+		TerrainTexture bTexture = new TerrainTexture(
+				loader.loadTexture("res/rock-terrain-texture.png"));
+
+		TerrainTexturePack texturePack = new TerrainTexturePack(
+				backgroundTexture, rTexture, gTexture, bTexture);
+
+		TerrainTexture blendMap = new TerrainTexture(
+				loader.loadTexture("res/blendmap.png"));
+
+		// create terrain
+
+		Terrain terrain1 = new Terrain(0, -1, loader, texturePack, blendMap);
+		Terrain terrain2 = new Terrain(1, -1, loader, texturePack, blendMap);
+		Terrain terrain3 = new Terrain(-1, -1, loader, texturePack, blendMap);
+
+		// create light
+
+		Light light = new Light(new Vector3f(0, 10, -20f),
+				new Vector3f(1, 1, 1));
+
+		// create camera
+
+		Camera camera = new Camera();
+		camera.getPosition().y += 1;
+
+		MasterRenderer renderer = new MasterRenderer();
+
+		// create stall entity
+
 		ModelData data = OBJFileLoader.loadOBJ("stall-model");
 		TexturedModel shopTexturedModel = new TexturedModel(loader.loadToVao(
 				data.getVertices(), data.getTextureCoords(), data.getNormals(),
 				data.getIndices()), new ModelTexture(
 				loader.loadTexture("res/stall-model-texture.png")));
-		ModelTexture texture = shopTexturedModel.getTexture();
-		texture.setShineDamper(10);
-		texture.setReflectivity(1);
-		texture.setAmbient(0.5f);
+		shopTexturedModel.getTexture().setShineDamper(10);
+		shopTexturedModel.getTexture().setReflectivity(1);
+		shopTexturedModel.getTexture().setAmbient(0.5f);
 
 		Entity shop = new Entity(shopTexturedModel, new Vector3f(0, 0, -50f),
 				0, 0, 0, 1);
 		shop.increaseRotation(0, 180, 0);
+		
+		//create palmtree
+		/*
+		data = OBJFileLoader.loadOBJ("palmtree-model");
+		TexturedModel palmTreeTexturedModel = new TexturedModel(loader.loadToVao(
+				data.getVertices(), data.getTextureCoords(), data.getNormals(),
+				data.getIndices()), new ModelTexture(
+				loader.loadTexture("res/palmtree-model-texture.png")));
+		palmTreeTexturedModel.getTexture().setShineDamper(10);
+		palmTreeTexturedModel.getTexture().setReflectivity(1);
+		palmTreeTexturedModel.getTexture().setAmbient(0.5f);
 
-		Light light = new Light(new Vector3f(0, 10, -20f),
-				new Vector3f(1, 1, 1));
-
-		Terrain terrain1 = new Terrain(0, -1, loader, texturePack, blendMap);
-		Terrain terrain2 = new Terrain(1, -1, loader, texturePack, blendMap);
-		Terrain terrain3 = new Terrain(-1, -1, loader, texturePack, blendMap);
+		Entity palmTree = new Entity(palmTreeTexturedModel, new Vector3f(0, 0, -50f),
+				0, 0, 0, 1);
+		*/
+		// create grass on terrain
 
 		data = OBJFileLoader.loadOBJ("grass-model");
 		TexturedModel grassTexturedModel = new TexturedModel(loader.loadToVao(
@@ -78,12 +114,10 @@ public class Main implements Runnable {
 		grassTexturedModel.getTexture().setHasTransparency(true);
 		grassTexturedModel.getTexture().setUseFakeLighting(true);
 
-		Camera camera = new Camera();
-		camera.getPosition().y += 1;
-
-		MasterRenderer renderer = new MasterRenderer();
+		// put all entities to draw in list
 
 		List<Entity> entities = new ArrayList<>();
+
 		Random random = new Random();
 		for (int i = 0; i < 500; i++) {
 			entities.add(new Entity(grassTexturedModel, new Vector3f(random
@@ -91,6 +125,9 @@ public class Main implements Runnable {
 					0, 0, 1));
 		}
 		entities.add(shop);
+//		entities.add(palmTree);
+
+		// main loop
 
 		while (isRunning) {
 			if (window.WindowShouldBeClosed())
