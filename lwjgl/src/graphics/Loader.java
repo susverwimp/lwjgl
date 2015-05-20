4cf513bf5173ef0e71a37f032db56e9eae42e6dc
susverwimp <sus.verwimp@hotmail.com>
2015-05-20 04:03:02 +0200

    mipmapping

--- lwjgl/src/graphics/Loader.java
+++ lwjgl/src/graphics/Loader.java
@@ -11,6 +11,7 @@ import javax.imageio.ImageIO;
 import models.RawModel;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
@@ -61,9 +62,11 @@ public class Loader {
 		
 		int result = GL11.glGenTextures();
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, result);
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
 		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, BufferUtils.createIntBuffer(data));
+		GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+		GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.4f);
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
 		textures.add(result);
 		return result;
