d10f6e3850afd66b555bd711525d94ff2824503d
Sus Verwimp <sus.verwimp@student.kuleuven.be>
2015-05-22 21:07:48 +0200

    added lamp with point light

--- lwjgl/src/lwjgl/Main.java
+++ lwjgl/src/lwjgl/Main.java
@@ -38,8 +38,17 @@ public class Main implements Runnable {
 		window.init();
 
 		Loader loader = new Loader();
+		
+		List<Entity> entities = new ArrayList<>();
+		List<Light> lights = new ArrayList<>();
+		
+		//create sun
+		Light sun = new Light(new Vector3f(1200, 1000, 1200), new Vector3f(
+				1, 1, 1));
+		lights.add(sun);
 
-		// create terrain textures and blendmap, used for multitexturing terrains
+		// create terrain textures and blendmap, used for multitexturing
+		// terrains
 
 		TerrainTexture backgroundTexture = new TerrainTexture(
 				loader.loadTexture("res/grass-terrain-texture.png"));
@@ -59,32 +68,18 @@ public class Main implements Runnable {
 		// create terrains
 
 		Terrain[][] terrains = new Terrain[3][3];
-		for(int i = 0; i<terrains.length; i++){
-			for(int j = 0; j<terrains[i].length; j++){
-				terrains[i][j] = new Terrain(i, j, loader, texturePack, blendMap, "heightmap");
+		for (int i = 0; i < terrains.length; i++) {
+			for (int j = 0; j < terrains[i].length; j++) {
+				terrains[i][j] = new Terrain(i, j, loader, texturePack,
+						blendMap, "heightmap");
 			}
 		}
-
-		// create light
-
-		Light light1 = new Light(new Vector3f(1200, 1000, 1200), new Vector3f(1,1,1));
-		Light light2 = new Light(new Vector3f(1200, 10, 2300), new Vector3f(2, 0, 0), new Vector3f(1,  0.01f, 0.002f));
-		Light light3 = new Light(new Vector3f(1200, 17, 2200), new Vector3f(0, 2, 2), new Vector3f(1, 0.01f, 0.002f));
-		Light light4 = new Light(new Vector3f(1200, 20, 2100), new Vector3f(2, 2, 0), new Vector3f(1, 0.01f, 0.002f));
-		
-		List<Light> lights = new ArrayList<>();
-		lights.add(light1);
-		lights.add(light2);
-		lights.add(light3);
-		lights.add(light4);
 
 		// create camera
 
 		Camera camera = new Camera(new Vector3f(1200, 1, 2400));
 
 		MasterRenderer renderer = new MasterRenderer();
-
-		List<Entity> entities = new ArrayList<>();
 
 		// create stall entity
 
@@ -99,35 +94,62 @@ public class Main implements Runnable {
 
 		float x = 1250;
 		float z = 2000;
-		int terrainColumn = (int) (x/Terrain.SIZE);
-		int terrainRow = (int) (z/Terrain.SIZE);
+		int terrainColumn = (int) (x / Terrain.SIZE);
+		int terrainRow = (int) (z / Terrain.SIZE);
 		float y = terrains[terrainColumn][terrainRow].getHeightOfTerrain(x, z);
-		Entity shop = new Entity(shopTexturedModel, new Vector3f(x,y,z), 0, 0, 0, 1);
+		Entity shop = new Entity(shopTexturedModel, new Vector3f(x, y, z), 0,
+				0, 0, 1);
 		shop.increaseRotation(0, 180, 0);
-		
+
 		// put all entities to draw in list
 		entities.add(shop);
 
-		//add fern
-		ModelTexture fernTextureAtlas = new ModelTexture(loader.loadTexture("res/fern-model-texture.png"));
+		// add fern
+		ModelTexture fernTextureAtlas = new ModelTexture(
+				loader.loadTexture("res/fern-model-texture.png"));
 		fernTextureAtlas.setNumberOfRows(2);
+
+		// create fern
 		
 		data = OBJFileLoader.loadOBJ("res/fern-model.obj");
-		TexturedModel fern = new TexturedModel(loader.loadToVao(
+		TexturedModel fernTexturedModel = new TexturedModel(loader.loadToVao(
 				data.getVertices(), data.getTextureCoords(), data.getNormals(),
 				data.getIndices()), fernTextureAtlas);
-		
+
 		Random random = new Random(676452);
-		for(int i =0; i<3000; i++){
-			if(i%2 == 0){
+		for (int i = 0; i < 3000; i++) {
+			if (i % 2 == 0) {
 				x = random.nextFloat() * terrains.length * Terrain.SIZE;
 				z = random.nextFloat() * terrains[0].length * Terrain.SIZE;
-				terrainColumn = (int) (x/Terrain.SIZE);
-				terrainRow = (int) (z/Terrain.SIZE);
-				y = terrains[terrainColumn][terrainRow].getHeightOfTerrain(x, z);
-				entities.add(new Entity(fern, random.nextInt(4), new Vector3f(x, y, z), 0, 0, 0, 1));
+				terrainColumn = (int) (x / Terrain.SIZE);
+				terrainRow = (int) (z / Terrain.SIZE);
+				y = terrains[terrainColumn][terrainRow]
+						.getHeightOfTerrain(x, z);
+				entities.add(new Entity(fernTexturedModel, random.nextInt(4), new Vector3f(
+						x, y, z), 0, 0, 0, 1));
 			}
 		}
+		
+		//create lamp with light
+		data = OBJFileLoader.loadOBJ("res/lamp-model.obj");
+		TexturedModel lampTexturedModel = new TexturedModel(loader.loadToVao(
+				data.getVertices(), data.getTextureCoords(), data.getNormals(),
+				data.getIndices()), new ModelTexture(
+				loader.loadTexture("res/lamp-model-texture.png")));
+
+		x = 1200;
+		z = 2300;
+		terrainColumn = (int) (x / Terrain.SIZE);
+		terrainRow = (int) (z / Terrain.SIZE);
+		y = terrains[terrainColumn][terrainRow].getHeightOfTerrain(x, z);
+		Entity lamp = new Entity(lampTexturedModel, new Vector3f(x, y, z), 0,
+				0, 0, 1);
+		lamp.getModel().getTexture().setUseFakeLighting(true);
+		entities.add(new Entity(lampTexturedModel, new Vector3f(x, y, z), 0,
+				0, 0, 1));
+		Light light2 = new Light(new Vector3f(x, y+15, z), new Vector3f(2,
+				0, 0), new Vector3f(1, 0.01f, 0.002f));
+		lights.add(light2);
 		
 		// main loop
 
@@ -137,8 +159,8 @@ public class Main implements Runnable {
 			for (Entity entity : entities) {
 				renderer.processEntity(entity);
 			}
-			for(int i = 0; i<terrains.length; i++){
-				for(int j=0; j<terrains[i].length; j++){
+			for (int i = 0; i < terrains.length; i++) {
+				for (int j = 0; j < terrains[i].length; j++) {
 					renderer.processTerrain(terrains[i][j]);
 				}
 			}
